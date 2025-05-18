e18f758e7afe347c87f1384842ce61588a871543
tolf <140963545+tolfdog@users.noreply.github.com>
2025-05-17 21:35:48 -0700

    add language negotiation

--- app/components/LinguiProvider.tsx
+++ app/components/LinguiProvider.tsx
@@ -1,10 +1,27 @@
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
 import { useEffect, useState } from "react";
+import { negotiateLanguages } from "@fluent/langneg";
+
+const AVAILABLE_LOCALES = Object.keys(
+  import.meta.glob("../locales/*/messages.po")
+).map((path) => path.replace(/..\/locales\/([^/]+)\/messages.po/, "$1"));
+
+function getNegotiatedBrowserLocale() {
+  const [negotiatedLocale] = negotiateLanguages(
+    navigator.languages,
+    AVAILABLE_LOCALES,
+    {
+      defaultLocale: "en",
+      strategy: "matching",
+    }
+  );
+  return negotiatedLocale;
+}
 
 async function loadCatalog(locale: string) {
   const { messages } = await import(`../locales/${locale}/messages.po`);
-  i18n.loadAndActivate({ locale, messages });
+  i18n.load(locale, messages);
 }
 
 export function LinguiProvider({
@@ -14,17 +31,18 @@ export function LinguiProvider({
   children: React.ReactNode;
   loadingPlaceholder?: React.ReactNode;
 }) {
-  const [isI18nReady, setIsI18nReady] = useState(false);
-  const [currentLocale] = useState("en");
+  const [isReady, setIsReady] = useState(false);
+  const [locale] = useState(() => getNegotiatedBrowserLocale());
 
   useEffect(() => {
     (async () => {
-      await loadCatalog(currentLocale);
-      setIsI18nReady(true);
+      await loadCatalog(locale);
+      i18n.activate(locale);
+      setIsReady(true);
     })();
-  }, [currentLocale]);
+  }, [locale, setIsReady]);
 
-  if (!isI18nReady) {
+  if (!isReady) {
     return loadingPlaceholder;
   }
 
