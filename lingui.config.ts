a1d8fcfc2f0b1d1dfd98fa241577259fb1e2c612
tolf <140963545+tolfdog@users.noreply.github.com>
2025-05-17 21:51:49 -0700

    reconfigure lingui

--- lingui.config.ts
+++ lingui.config.ts
@@ -3,14 +3,10 @@ import { defineConfig } from "@lingui/cli";
 export default defineConfig({
   locales: ["en"],
   sourceLocale: "en",
-  compileNamespace: "ts",
   catalogs: [
     {
       path: "<rootDir>/app/locales/{locale}/messages",
       include: ["app"],
     },
   ],
-  fallbackLocales: {
-    default: "en",
-  },
 });
