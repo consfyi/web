3cd8c033949a910ef8c587808842aeadcec689c0
tolf <140963545+tolfdog@users.noreply.github.com>
2025-05-26 18:53:37 -0700

    add locale setting

--- app/components/LinguiProvider.tsx
+++ app/components/LinguiProvider.tsx
@@ -8,9 +8,25 @@ const AVAILABLE_LOCALES = Object.keys(
   import.meta.glob("../locales/*/messages.po")
 ).map((path) => path.replace(/..\/locales\/([^/]+)\/messages.po/, "$1"));
 
+const LOCALE_KEY = "fbl:locale";
+
 function getNegotiatedBrowserLocale(): Locale {
+  const requestedLocales: string[] = [];
+
+  if (typeof window !== "undefined") {
+    const searchParams = new URLSearchParams(location.search);
+    requestedLocales.push(...searchParams.getAll("lang"));
+
+    const storedLocale = window.localStorage.getItem(LOCALE_KEY);
+    if (storedLocale != null) {
+      requestedLocales.push(storedLocale);
+    }
+  }
+
+  requestedLocales.push(...navigator.languages);
+
   const [negotiatedLocale] = negotiateLanguages(
-    navigator.languages,
+    requestedLocales,
     AVAILABLE_LOCALES,
     {
       defaultLocale: "en",
@@ -23,6 +39,7 @@ function getNegotiatedBrowserLocale(): Locale {
 async function loadAndActivate(locale: string) {
   const { messages } = await import(`../locales/${locale}/messages.po`);
   i18n.loadAndActivate({ locale, messages });
+  window.localStorage.setItem(LOCALE_KEY, locale);
 }
 
 const INITIAL_LOCALE = getNegotiatedBrowserLocale();
@@ -32,7 +49,7 @@ const useInitialLoad = hookifyPromise(loadAndActivate(INITIAL_LOCALE));
 export const LinguiContext = createContext({
   locale: INITIAL_LOCALE,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  setLocale(locale: Locale) {},
+  setLocale(_locale: Locale) {},
 });
 
 export default function LinguiProvider(props: Omit<I18nProviderProps, "i18n">) {
