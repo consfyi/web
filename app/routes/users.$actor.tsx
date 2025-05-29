2895ef719d3ec522bc77814774b48ca2a2760ad9
tolf <140963545+tolfdog@users.noreply.github.com>
2025-05-28 19:26:35 -0700

    load attending labels lazily

--- app/routes/users.$actor.tsx
+++ app/routes/users.$actor.tsx
@@ -1,16 +1,18 @@
-import type { ActorIdentifier } from "@atcute/lexicons";
+import type { ActorIdentifier, Did } from "@atcute/lexicons";
 import { Plural, Trans } from "@lingui/react/macro";
 import {
   Anchor,
   Box,
+  Center,
   Divider,
   Group,
+  Loader,
   SimpleGrid,
   Text,
   Title,
 } from "@mantine/core";
 import { useParams } from "@remix-run/react";
-import { addDays, isAfter } from "date-fns";
+import { Suspense } from "react";
 import Avatar from "~/components/Avatar";
 import { ConRow } from "~/components/ConsList";
 import {
@@ -20,18 +22,9 @@ import {
   useSelf,
 } from "~/hooks";
 
-export default function Index() {
-  const { actor } = useParams();
-
+function Attending({ did }: { did: Did }) {
   const self = useSelf();
-  const profile = useProfile(actor as ActorIdentifier);
-  const labels = useProfileLabels(profile != null ? profile.did : undefined)!;
-
-  if (profile == null) {
-    throw new Response(null, {
-      status: 404,
-    });
-  }
+  const labels = useProfileLabels(did);
 
   const selfLabelsSet =
     self != null ? new Set(self.labels!.map((label) => label.val)) : null;
@@ -49,6 +42,71 @@ export default function Index() {
     selfLabelsSet != null
       ? filteredCons.filter((con) => !selfLabelsSet.has(con.identifier))
       : [];
+
+  return (
+    <Box>
+      <Title order={2} size="h5" fw={500} mb="sm">
+        <Trans>Attending</Trans>{" "}
+        <Text size="sm" span>
+          {filteredCons.length}
+        </Text>
+      </Title>
+
+      <SimpleGrid cols={{ base: 1, lg: 3 }} mb="sm">
+        {knownCons.map((con) => (
+          <ConRow
+            key={con.identifier}
+            con={con}
+            showMonthInIcon
+            showEndDateOnly
+            showLocation={false}
+            showFollowed={false}
+            showLikeButton={false}
+          />
+        ))}
+      </SimpleGrid>
+      {unknownCons.length > 0 ? (
+        <>
+          <Divider
+            label={
+              <Plural
+                value={unknownCons.length}
+                one="# con you aren’t attending"
+                other="# cons you aren’t attending"
+              />
+            }
+            labelPosition="left"
+            mb="sm"
+          />
+          <SimpleGrid cols={{ base: 1, lg: 3 }} mb="sm">
+            {unknownCons.map((con) => (
+              <ConRow
+                key={con.identifier}
+                con={con}
+                showMonthInIcon
+                showEndDateOnly
+                showLocation={false}
+                showFollowed={false}
+                showLikeButton={false}
+              />
+            ))}
+          </SimpleGrid>
+        </>
+      ) : null}
+    </Box>
+  );
+}
+
+export default function Index() {
+  const { actor } = useParams();
+
+  const profile = useProfile(actor as ActorIdentifier);
+
+  if (profile == null) {
+    throw new Response(null, {
+      status: 404,
+    });
+  }
 
   return (
     <Box p="sm">
@@ -73,56 +131,20 @@ export default function Index() {
         </Group>
       </Box>
 
-      <Box>
-        <Title order={2} size="h5" fw={500} mb="sm">
-          <Trans>Attending</Trans>{" "}
-          <Text size="sm" span>
-            {filteredCons.length}
-          </Text>
-        </Title>
-
-        <SimpleGrid cols={{ base: 1, lg: 3 }} mb="sm">
-          {knownCons.map((con) => (
-            <ConRow
-              key={con.identifier}
-              con={con}
-              showMonthInIcon
-              showEndDateOnly
-              showLocation={false}
-              showFollowed={false}
-              showLikeButton={false}
-            />
-          ))}
-        </SimpleGrid>
-        {unknownCons.length > 0 ? (
-          <>
-            <Divider
-              label={
-                <Plural
-                  value={unknownCons.length}
-                  one="# con you aren’t attending"
-                  other="# cons you aren’t attending"
-                />
-              }
-              labelPosition="left"
-              mb="sm"
-            />
-            <SimpleGrid cols={{ base: 1, lg: 3 }} mb="sm">
-              {unknownCons.map((con) => (
-                <ConRow
-                  key={con.identifier}
-                  con={con}
-                  showMonthInIcon
-                  showEndDateOnly
-                  showLocation={false}
-                  showFollowed={false}
-                  showLikeButton={false}
-                />
-              ))}
-            </SimpleGrid>
-          </>
-        ) : null}
-      </Box>
+      <Suspense
+        fallback={
+          <Box>
+            <Title order={2} size="h5" fw={500} mb="sm">
+              <Trans>Attending</Trans>
+            </Title>
+            <Center p="lg">
+              <Loader />
+            </Center>
+          </Box>
+        }
+      >
+        <Attending did={profile.did!} />
+      </Suspense>
     </Box>
   );
 }
