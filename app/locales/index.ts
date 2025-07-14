a6538822641d883fb4463f831ffdd042ba1a4465
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-13 23:24:24 -0700

    fix type decl

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -1,10 +1,5 @@
 export default {
-  // @ts-ignore
   "en-US": () => import("./en/messages.po"),
-
-  // @ts-ignore
   "en-GB": () => import("./en/messages.po"),
-
-  // @ts-ignore
   ja: () => import("./ja/messages.po"),
 } as Record<string, () => Promise<{ messages: any }>>;
--- app/vite-env.d.ts
+++ app/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+declare module "*.po" {
+  const messages: { [key: string]: string };
+  export { messages };
+}
