9ce41ffa7a2269d5817f52f7122d4e7c781dcb30
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-16 10:40:41 -0700

    show tooltips on flag

--- app/components/Flag.tsx
+++ app/components/Flag.tsx
@@ -1,5 +1,5 @@
 import { useLingui } from "@lingui/react/macro";
-import { BoxProps, Image, Box } from "@mantine/core";
+import { BoxProps, Image, Box, Tooltip } from "@mantine/core";
 import { useMemo } from "react";
 import classes from "./Flag.module.css";
 
@@ -18,21 +18,25 @@ export default function Flag({
     [t]
   );
 
-  const countryName = useMemo(() => countryNames.of(country), [country]);
+  const countryName = useMemo(
+    () => countryNames.of(country),
+    [country, countryNames]
+  );
 
   return (
-    <Box
-      component="span"
-      className={`${classes.flag} ${circular ? classes.circular : ""}`}
-      h={size}
-      w={circular ? size : (size / 3) * 4}
-      {...props}
-    >
-      <Image
-        src={`https://cdn.jsdelivr.net/npm/flagpack@latest/flags/4x3/${country.toLowerCase()}.svg`}
-        title={countryName}
-        alt={countryName}
-      />
-    </Box>
+    <Tooltip label={countryName}>
+      <Box
+        component="span"
+        className={`${classes.flag} ${circular ? classes.circular : ""}`}
+        h={size}
+        w={circular ? size : (size / 3) * 4}
+        {...props}
+      >
+        <Image
+          src={`https://cdn.jsdelivr.net/npm/flagpack@latest/flags/4x3/${country.toLowerCase()}.svg`}
+          alt={countryName}
+        />
+      </Box>
+    </Tooltip>
   );
 }
