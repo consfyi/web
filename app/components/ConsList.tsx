d5c100298d9a0a7d80f1e7c9e072347d4cdcdb66
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-16 17:47:16 -0700

    add map

--- app/components/ConsList.tsx
+++ app/components/ConsList.tsx
@@ -70,7 +70,6 @@ import {
   sorted,
   toArray,
 } from "iter-fns";
-import "leaflet/dist/leaflet.css";
 import { Fragment, lazy, Suspense, useMemo, useState } from "react";
 import { Link } from "react-router";
 import regexpEscape from "regexp.escape";
@@ -99,6 +98,8 @@ export function ConRow({
   showLocation,
   showFollowed,
   showLikeButton,
+  showBigIcon,
+  showDuration,
 }: {
   con: ConWithPost;
   showMonthInIcon: boolean;
@@ -106,6 +107,8 @@ export function ConRow({
   showLocation: boolean;
   showFollowed: boolean;
   showLikeButton: boolean;
+  showBigIcon: boolean;
+  showDuration: boolean;
 }) {
   const isAttending = con.post.viewer?.like != null;
   const { data: followedConAttendees } = useFollowedConAttendeesDLE();
@@ -144,6 +147,10 @@ export function ConRow({
 
   const now = useNow();
   const active = isAfter(now, con.start) && !isAfter(now, con.end);
+  const dateRange = dateTimeFormat.formatRange(
+    reinterpretAsLocalDate(con.start),
+    reinterpretAsLocalDate(subDays(con.end, 1))
+  );
 
   return (
     <Group gap="xs" wrap="nowrap">
@@ -157,32 +164,40 @@ export function ConRow({
           disabled={!active}
           zIndex={2}
         >
-          <ThemeIcon
-            size="xl"
-            variant="light"
-            color={
-              ["red", "orange", "yellow", "green", "blue", "indigo", "violet"][
-                getDay(reinterpretAsLocalDate(con.start))
-              ]
-            }
-          >
-            <Stack gap={0}>
-              <Text size="md" ta="center" fw={500}>
-                {showMonthInIcon
-                  ? i18n.date(reinterpretAsLocalDate(con.start), {
-                      month: "short",
-                    })
-                  : i18n.date(reinterpretAsLocalDate(con.start), {
-                      weekday: "short",
-                    })}
-              </Text>
-              <Text size="xs" ta="center" fw={500}>
-                {i18n.date(reinterpretAsLocalDate(con.start), {
-                  day: "numeric",
-                })}
-              </Text>
-            </Stack>
-          </ThemeIcon>
+          {showBigIcon ? (
+            <ThemeIcon
+              size="xl"
+              variant="light"
+              color={
+                [
+                  "red",
+                  "orange",
+                  "yellow",
+                  "green",
+                  "blue",
+                  "indigo",
+                  "violet",
+                ][getDay(reinterpretAsLocalDate(con.start))]
+              }
+            >
+              <Stack gap={0}>
+                <Text size="md" ta="center" fw={500}>
+                  {showMonthInIcon
+                    ? i18n.date(reinterpretAsLocalDate(con.start), {
+                        month: "short",
+                      })
+                    : i18n.date(reinterpretAsLocalDate(con.start), {
+                        weekday: "short",
+                      })}
+                </Text>
+                <Text size="xs" ta="center" fw={500}>
+                  {i18n.date(reinterpretAsLocalDate(con.start), {
+                    day: "numeric",
+                  })}
+                </Text>
+              </Stack>
+            </ThemeIcon>
+          ) : null}
         </Indicator>
       </Anchor>
       <Box style={{ minWidth: 0 }}>
@@ -256,19 +271,19 @@ export function ConRow({
           {!showEndDateOnly ? (
             <Text span>
               <IconCalendar title={t`Date`} size={12} />{" "}
-              <Trans context="[start date]-[end date] ([duration] days long)">
-                {dateTimeFormat.formatRange(
-                  reinterpretAsLocalDate(con.start),
-                  reinterpretAsLocalDate(subDays(con.end, 1))
-                )}{" "}
-                (
-                <Plural
-                  value={differenceInDays(con.end, con.start)}
-                  one="# day long"
-                  other="# days long"
-                />
-                )
-              </Trans>
+              {showDuration ? (
+                <Trans context="[start date]-[end date] ([duration] days long)">
+                  {dateRange} (
+                  <Plural
+                    value={differenceInDays(con.end, con.start)}
+                    one="# day long"
+                    other="# days long"
+                  />
+                  )
+                </Trans>
+              ) : (
+                dateRange
+              )}
             </Text>
           ) : (
             <Text span>
@@ -422,6 +437,8 @@ function ConsByDate({
                   showLocation
                   showFollowed
                   showLikeButton
+                  showBigIcon
+                  showDuration
                 />
               </Box>
             );
@@ -518,6 +535,8 @@ function ConsBy({ cons }: { cons: ConWithPost[] }) {
               showLocation
               showFollowed
               showLikeButton
+              showBigIcon
+              showDuration
             />
           </Box>
         );
@@ -1269,17 +1288,17 @@ function Filters({
                   </Center>
                 ),
               },
-              // {
-              //   value: "map",
-              //   label: (
-              //     <Center style={{ gap: 6 }}>
-              //       <IconMap size={14} />
-              //       <Text span size="xs" visibleFrom="lg">
-              //         <Trans>Map</Trans>
-              //       </Text>
-              //     </Center>
-              //   ),
-              // },
+              {
+                value: "map",
+                label: (
+                  <Center style={{ gap: 6 }}>
+                    <IconMap size={14} />
+                    <Text span size="xs" visibleFrom="lg">
+                      <Trans>Map</Trans>
+                    </Text>
+                  </Center>
+                ),
+              },
             ]}
             size="xs"
           />
@@ -1615,7 +1634,7 @@ export default function ConsList({
           </Center>
         }
       >
-        {filteredCons.length > 0 ? (
+        {filteredCons.length > 0 || viewOptions.layout.type == "map" ? (
           viewOptions.layout.type == "calendar" ? (
             <CalendarLayout
               cons={filteredCons}
--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,26 +1,113 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { Box, useComputedColorScheme, useMantineTheme } from "@mantine/core";
+import { IconMapPinFilled } from "@tabler/icons-react";
+import { getDay } from "date-fns";
+import { RMap, RMarker, RPopup } from "maplibre-react-components";
+import "maplibre-theme/icons.default.css";
+import "maplibre-theme/modern.css";
+import { useState } from "react";
 import { ConWithPost } from "~/hooks";
+import ConDetails from "./ConDetails";
+import { ConRow } from "./ConsList";
+
+function Pin({
+  con,
+  showPopup,
+  onClick,
+}: {
+  con: ConWithPost;
+  showPopup: boolean;
+  onClick?(e: MouseEvent): void;
+}) {
+  const theme = useMantineTheme();
+
+  const rawLatLng = con.geocoded?.latLng;
+  if (rawLatLng == null) {
+    return null;
+  }
+  const [rawLat, rawLng] = rawLatLng;
+  const lat = parseFloat(rawLat);
+  const lng = parseFloat(rawLng);
+
+  const color = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+  ][getDay(con.start)];
+
+  const colors = theme.variantColorResolver({
+    theme,
+    color,
+    variant: "filled",
+  });
+
+  return (
+    <>
+      <RMarker latitude={lat} longitude={lng} onClick={onClick}>
+        <IconMapPinFilled
+          size={32}
+          color={colors.background}
+          style={{
+            stroke: colors.color,
+            marginTop: "-100%",
+          }}
+        />
+      </RMarker>
+      {showPopup ? (
+        <RPopup latitude={lat} longitude={lng} offset={[0, -14]}>
+          <ConRow
+            con={con}
+            showMonthInIcon
+            showEndDateOnly={false}
+            showLocation={false}
+            showFollowed
+            showLikeButton
+            showBigIcon={false}
+            showDuration={false}
+          />
+        </RPopup>
+      ) : null}
+    </>
+  );
+}
 
 export default function Map({ cons }: { cons: ConWithPost[] }) {
+  const colorScheme = useComputedColorScheme();
+  const [selected, setSelected] = useState<string | null>();
+
   return (
-    <MapContainer center={[0, 0]} zoom={2} style={{ height: "800px" }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {cons.flatMap((con) => {
-        const latLng = con.geocoded?.latLng;
-        if (latLng == null) {
-          return [];
+    <Box px="xs">
+      <RMap
+        onClick={() => {
+          setSelected(null);
+        }}
+        className={colorScheme}
+        mapStyle={
+          colorScheme == "dark"
+            ? "https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json"
+            : "https://tiles.stadiamaps.com/styles/alidade_smooth.json"
         }
-        const [lat, lng] = latLng;
-        return [
-          <Marker
-            key={con.slug}
-            position={[parseFloat(lat), parseFloat(lng)]}
-          />,
-        ];
-      })}
-    </MapContainer>
+        initialCenter={[0, 0]}
+        initialZoom={1}
+        style={{ height: "800px" }}
+      >
+        {cons.map((con) => {
+          return (
+            <Pin
+              key={con.slug}
+              con={con}
+              showPopup={con.slug == selected}
+              onClick={(e) => {
+                e.stopPropagation();
+                setSelected(con.slug);
+              }}
+            />
+          );
+        })}
+      </RMap>
+    </Box>
   );
 }
