27270ed53a07263e7e8d756346a6119ae414a50e
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-21 21:35:59 -0700

    absurd should return T

--- app/absurd.ts
+++ app/absurd.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function absurd(x: never): never {
+export default function absurd<T>(x: never): T {
   throw new Error("absurd");
 }
