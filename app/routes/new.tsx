5e4cc4a8b335decc3ae453588e86d99dcd6924ed
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-25 18:22:06 -0700

    add templated con

--- app/routes/new.tsx
+++ app/routes/new.tsx
@@ -1,9 +1,24 @@
 import { useLingui } from "@lingui/react/macro";
 import { Container } from "@mantine/core";
 import { useEffect } from "react";
-import Editor from "~/components/Editor";
+import Editor, { Entry } from "~/components/Editor";
+import type { Route } from "./+types/new";
 
-export default function New() {
+export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+  const url = new URL(request.url);
+  const from = url.searchParams.get("from");
+
+  const res = await fetch(`https://data.cons.fyi/cons/${from}.json`);
+  if (!res.ok) {
+    return null;
+  }
+  const con = await res.json();
+  return con;
+}
+
+export default function New({ loaderData }: Route.ComponentProps) {
+  const entry = loaderData as Entry;
+
   const { t } = useLingui();
 
   useEffect(() => {
@@ -12,7 +27,7 @@ export default function New() {
 
   return (
     <Container size="lg" p="sm">
-      <Editor />
+      <Editor entry={entry ?? undefined} />
     </Container>
   );
 }
