7b8ed3126cb1bdaf8042c68e1938a4335f0170a4
tolf <140963545+tolfdog@users.noreply.github.com>
2025-08-01 12:54:03 -0700

    hardcode the header height, it's broken :(

--- app/components/HeaderHeightProvider.tsx
+++ app/components/HeaderHeightProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, use } from "react";
 
-const HeaderHeightContext = createContext(0);
+const HeaderHeightContext = createContext(61);
 
 export function useHeaderHeight() {
   return use(HeaderHeightContext);
